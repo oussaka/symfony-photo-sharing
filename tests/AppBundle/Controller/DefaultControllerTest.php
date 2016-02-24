1e46bb1eff48a8bb43eb7421a5218a947942e60c
Raúl Continente <raulcontinente@gmail.com>
2016-02-24 01:51:45 +0100

    adding a test unit

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,50 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * @dataProvider getPublicUrls
+     */
+    public function testPublicUrls($url)
     {
-        $client = static::createClient();
+        $client = self::createClient();
+        $client->request('GET', $url);
 
-        $crawler = $client->request('GET', '/');
+        $this->assertTrue(
+            $client->getResponse()->isSuccessful(),
+            sprintf('The %s public URL loads correctly.', $url)
+        );
+    }
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    /**
+     * @dataProvider getSecureUrls
+     */
+    public function testSecureUrls($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+
+        $this->assertEquals(
+            'http://localhost/login',
+            $client->getResponse()->getTargetUrl(),
+            sprintf('The %s secure URL redirects to the login form.', $url)
+        );
+    }
+
+    public function getPublicUrls()
+    {
+        return array(
+            array('/'),
+            array('/explore'),
+        );
+    }
+
+    public function getSecureUrls()
+    {
+        return array(
+            array('/upload'),
+            array('/photos/19/edit')
+        );
     }
 }
